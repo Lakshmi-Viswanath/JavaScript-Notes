23dce858c535beeb75592313f19d441b78bde703
Lakshmi-Viswanath <lakshmiv03@gmail.com>
2024-04-17 11:39:01 +0530

    hof

---
+++ coercion/abstractoperation.js
@@ -0,0 +1,10 @@
+//ToNumber abstract operation
+console.log(2 - 9); // -7
+console.log(2 - null); // 2
+console.log(2 - undefined); // NaN
+
+console.log(1 - "123"); //-122
+console.log(1 - "ar123"); //NaN
+
+//ToPrimitive abstract operation
+
--- functions/arrayfunction.js
+++ functions/arrayfunction.js
@@ -21,6 +21,7 @@ let res = array.map(print);
 console.log(res);
 
 //sort function
+//-----------------------------------
 let a = [0,66,1,888,23,98,456];
 
 console.log(a.sort()); //0,1,23,456,66,888,98
@@ -31,3 +32,30 @@ a.sort((a,b) =>{
 
 console.log(a); //0,1,23,66,98,456,888
 
+//filter function
+//---------------------------------------
+function isOdd(x){
+    return (x % 2 !== 0)
+}
+
+let result3 = array.filter(isOdd);
+console.log(result3); //1,3,5
+
+//reduce function
+//--------------------------------------
+function sum(preValue,currentValue){
+    return preValue + currentValue
+}
+
+let result4 = array.reduce(sum);
+console.log(result4); //21
+
+let obj = [{price:123,name:"samsung"},{price:123,name:"iPhone"},{price:123,name:"laptop"}];
+function add(pValue,cValue){
+    console.log(pValue);
+    let newPrice = pValue.price + cValue.price;
+    return {price:newPrice};
+}
+
+let result5 = obj.reduce(add);
+console.log(result5); //21
